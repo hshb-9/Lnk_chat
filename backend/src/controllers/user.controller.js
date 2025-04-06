7fdd72ff06a587ed110796417ddca329d16290b1
hshb-9 <anonymousnot9o@gmail.com>
2025-04-06 06:54:27 +0530

    Fix: Add missing backend files and logic issues

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,11 +1,4 @@
 import User from "../models/user.model.js";
-// If this is how you're exporting in controller.js:
-module.exports = {
-  getUsersForSidebar,
-};
-
-// Then in user.route.js, change the import to:
-const { getUsersForSidebar } = require('../controllers/user.controller');
 
 export const getUsers = async (req, res) => {
   try {
